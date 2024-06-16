4aa7fe383b96ea69ba4db4bf30e7c452d5a7f4e4
Manish Kumar Namdeo <mknamdeo@gmail.com>
2024-06-16 16:33:32 +0530

    Styles added

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,10 @@
+import classes from './Post.module.css'
 function Post(props)
 {
     return(
-        <div>
-            <p>{props.author}</p>
-            <p>{props.body}</p>
+        <div className={classes.post}>
+            <p className={classes.author}>{props.author}</p>
+            <p className={classes.body}>{props.body}</p>
         </div>
     )
 }
