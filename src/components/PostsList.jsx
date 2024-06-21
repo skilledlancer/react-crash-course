d1ca792fdb3b69383c97f42be3619d55f4c1b6de
Manish Kumar Namdeo <mknamdeo@gmail.com>
2024-06-21 22:49:35 +0530

    Event Listner Example

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,14 @@
 import Post from './Post'
+import NewPost from './NewPost';
 import classes from './PostsList.module.css'
 function PostsList(props) {
-return(<ul className={classes.posts}>
+return(<>
+<NewPost />
+<ul className={classes.posts}>
       <Post author="Manish" body="Learning React"/>
       <Post author="Sheetal" body="Awesome Journey"/>
       <Post author="Sparsh" body="Lets Enjoy"/>
-</ul>);
+</ul></>);
 }
 
 export default PostsList;
