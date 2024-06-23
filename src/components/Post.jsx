39f05af79022f43275265446299f1dc0e0b2da32
Manish Kumar Namdeo <mknamdeo@gmail.com>
2024-06-23 13:49:36 +0530

    Lifting Stage up in hierarchy

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,6 @@
 import classes from './Post.module.css'
-function Post(props)
-{
-    return(
+function Post(props) {
+    return (
         <li className={classes.post}>
             <p className={classes.author}>{props.author}</p>
             <p className={classes.body}>{props.body}</p>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,26 @@
 import Post from './Post'
 import NewPost from './NewPost';
 import classes from './PostsList.module.css'
+import { useState } from 'react';
+
+
 function PostsList(props) {
-return(<>
-<NewPost />
-<ul className={classes.posts}>
-      <Post author="Manish" body="Learning React"/>
-      <Post author="Sheetal" body="Awesome Journey"/>
-      <Post author="Sparsh" body="Lets Enjoy"/>
-</ul></>);
+      const [enteredBody, setEnteredBody] = useState('');
+      const [enteredAuthor, setEnteredAuthor] = useState('');
+
+      function bodyChangeHandler(event) {
+            setEnteredBody(event.target.value);
+      }
+      function authorChangeHandler(event) {
+            setEnteredAuthor(event.target.value);
+      }
+      return (<>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            <ul className={classes.posts}>
+                  <Post author={enteredAuthor} body={enteredBody} />
+                  <Post author="Sheetal" body="Awesome Journey" />
+                  <Post author="Sparsh" body="Lets Enjoy" />
+            </ul></>);
 }
 
 export default PostsList;
